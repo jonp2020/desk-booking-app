a015ed99ad5e8f14902f1e1c6a98179bb6de351f
Matthew Richardson <matthewrichardson@YLQ5H4GP7P.local>
2022-10-24 15:40:34 +0100

    Adding test library

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,7 @@
-const express = require("express")
-const app = express()
+const app = require("./app.js")
 const dotenv = require("dotenv").config()
 
-
-
 const port = process.env.PORT || 4000
-
-
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
