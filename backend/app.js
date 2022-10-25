85475ddbbdf3830eaf87ab959dfdafb9f54a5984
Payapulli <joshua.payapulli@gmail.com>
2022-10-25 17:00:32 +0100

    Working get_reservation method

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,6 @@ const app = express()
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use("/api/users", require("./routes/users"));
+app.use("/api/reservations", require("./routes/reservations"));
 
 module.exports = app;
