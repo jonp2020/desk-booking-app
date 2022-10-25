cf82835d7b87fabd3ea34a752c5163f831f71d33
jonp2020 <62655589+jonp2020@users.noreply.github.com>
2022-10-25 15:26:20 +0100

    Added dotenv jest connection and tests, removed console logs

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,76 +1,60 @@
-// const mongoose = require("mongoose");
-// const request = require("supertest");
-// const app = require("../app.js");
-// const connect_db = require("../db.js");
+const { exportAllDeclaration } = require("@babel/types");
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../app.js");
+const connect_db = require("../db.js");
 
-// beforeEach(() => {
-//   connect_db()
-// });
+beforeAll(async () => {
+  await connect_db()
+});
 
-// afterAll(() => {
-//   mongoose.connection.close()
-// });
 
-// mongoose
-//     .connect( process.env.MONGO_URI)
-//     .then(() => {
-//         mongoose.set('useFindAndModify', false)
-//     })
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
-// afterAll (async () => {
-//   await mongoose.connection.close();
-// })
 
-// describe("Testing the API GET desks", () => {
-//     test("It should response the GET method", () => {
-//       return request(app)
-//         .get("/api/desks")
-//         .then(response => {
-//           expect(response.statusCode).toBe(200);
-//           done();
-//         });
-//     });
-//   });
+describe("Testing the API GET desks", () => {
+    test("It should response the GET method", () => {
+      return request(app)
+        .get("/api/desks")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+        });
+    });
+  });
 
-//   describe("Testing the API POST desks", () => {
-//     test("It should response the POST method", () => {
-//       return request(app)
-//         .post("/api/POST")
-//         .then(response => {
-//           expect(response.statusCode).toBe(201);
-//           done();
-//         });
-//     });
-//   });
+  describe("Testing the API POST desks", () => {
+    test("It should response the POST method", () => {
+      return request(app)
+        .post("/api/POST")
+        .then(response => {
+          expect(response.statusCode).toBe(201);
+        });
+    });
+  });
 
-  // describe("Testing the API DELETE desks", () => {
-  //   test("It should response the DELETE method", () => {
-  //     return request(app)
-  //       .delete("/api/DELETE")
-  //       .then(response => {
-  //         expect(response.statusCode).toBe(202);
-  //         done();
-  //       });
-  //   });
-  // });
+  describe("Testing the API DELETE desks", () => {
+    test("It should response the DELETE method", () => {
+      return request(app)
+        .delete("/api/DELETE")
+        .then(response => {
+          expect(response.statusCode).toBe(202);
+        });
+    });
+  });
+  
 
-  // describe("Testing get_users", () => {
-  //   test("It should respond with status 200", () => {
-  //     return request(app)
-  //       .get("/api/users")
-  //       .expect(200);
-  //       // .then(response => {
-  //       //   //expect(response.statusCode).toBe(200);
-  //       //   expect(200);
-  //       //   done();
-  //       // });
-  //   });
-  // });
+  describe("GET /api/users", () => {
+    test("200 status code", () => {
+      return request(app)
+      .get("/api/users")
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      })
+    })
+  })
 
-  // describe("GET /api/users", () => {
-  //   // Connect to Database in here and get it running
-  //   // Stop frontend test from complaining
-  //   test("200 status code", () => {
-  //     return request(app).get("/api/users").expect(200);
-  //   });
-  // });
+
+ 
+  
--- backend/controllers/controller_database.js
+++ backend/controllers/controller_database.js
@@ -1,13 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const Users = require("../models/model_users");
 
-const get_users = asyncHandler( async (request, response) => {
-
+const get_users = asyncHandler(async (request, response) => {
     const users = await Users.find();
-    console.log("Responded with Code", response.statusCode);
-    console.log("Users:", users)
-    response.status(200);
-
+    response.status(200).json(users);
 });
 
 module.exports = {get_users};
