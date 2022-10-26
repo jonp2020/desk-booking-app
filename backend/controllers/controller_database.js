6190d9d8ad2283a72e553eea7bdf057b164dc768
jonp2020 <62655589+jonp2020@users.noreply.github.com>
2022-10-26 10:44:04 +0100

    Add functionality to post a reservation, delete a reservation with tests

--- backend/controllers/controller_database.js
+++ backend/controllers/controller_database.js
@@ -9,15 +9,27 @@ const get_users = asyncHandler(async (request, response) => {
 });
 
 const get_reservations = asyncHandler(async (request, response) => {
-    const {office, date, time} = request.body
-    const reservations = await Reservations.find(office, date, time);
+    const reservations = await Reservations.find(request.body);
+    // Get the office layout from DB
+    // loop through and create a new object which includes the bookings
+    // save that into some kind of new object and send that back
+
+
     response.status(200).json(reservations);
 });
 
 const post_reservations = asyncHandler(async (request, response) => {
-    const {office, seat_no, table_no, monitor, date, time} = request.body
-    const reservation = await Reservations.create(office, seat_no, table_no, monitor, date, time);
+    const reservation = await Reservations.create(request.body);
     response.status(201).json(reservation);
 });
+const delete_reservation = asyncHandler(async (request, response) => {
+    const reservationToDelete = await Reservations.find(request.body);
+    const reservation = await Reservations.deleteOne(reservationToDelete[0]._id);
+    if (reservation.acknowledged) {
+        response.status(202).json({message: "Your booking has been removed."});
+    } else {
+        response.status(400).json({message: "Booking not deleted. Please try again."})
+    }
+});
 
-module.exports = {get_users, get_reservations, post_reservations};
+module.exports = {get_users, get_reservations, post_reservations, delete_reservation};
--- backend/models/model_reservations.js
+++ backend/models/model_reservations.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const reservations_schema = new Schema({
     office: {type:String, required:true},
-    seat_no: {type:Number, required:true},
-    table_no: {type:Number, required:true},
-    monitor: {type:Boolean, required:true},
-    date: {type:Date, required:true},
+    name: {type:String, required:true},
+    seat_no: {type:String, required:true},
+    table_no: {type:String, required:true},
+    monitor: {type:String, required:true},
+    date: {type:String, required:true},
     time: {type:String, required:true}
 },{timestamps:true});
 
